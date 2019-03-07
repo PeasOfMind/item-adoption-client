fccf3bd2f00c80fa66b5ab5095dd7523b44b20c3
Yeqing Liu <40677349+PeasOfMind@users.noreply.github.com>
2019-03-06 20:53:26 -0500

    made styling changes to keep buttons and forms consistent

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -29,8 +29,7 @@ export function LandingPage(props){
                 <p>With Item Adoption, take the things that you were about to throw away and find ways to repurpose them, find places to donate them, or give/sell to others in your area. You can also look for items that are used and give them a new home!</p>
             </section>
             <section className="direct-to-signup">
-            <Link to="/register">Sign Up</Link>
-                {/* <button type="button" onClick={handleClick} id="button-to-signup">Sign up to get started</button> */}
+            <Link to="/register"><button type="button" id="button-to-signup">Sign up to get started</button></Link>
             </section>
         </main>
     )
