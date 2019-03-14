14e7b328598110752b609537541591761945b2af
Yeqing Liu <40677349+PeasOfMind@users.noreply.github.com>
2019-03-14 08:04:21 -0400

    added test to check that submit is working on user-form

--- src/components/user-form.test.js
+++ src/components/user-form.test.js
@@ -1,10 +1,45 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 
-import UserForm from './user-form';
+import {UserForm} from './user-form';
 
 describe('<UserForm />', () => {
+    let submitting, touched, error, callback;
+    const testUser = 'user123';
+    const testPass = 'pass456';
+    beforeEach(() => {
+        submitting = false;
+		touched = false;
+		error = null;
+        callback = jest.fn();
+    })
+
+    const buildSubject = () => {
+        const props = {
+            submitting,
+            handleSubmit: callback,
+            fields: {
+                username: {
+                    value: testUser,
+                    touched,
+                    error
+                },
+                password: {
+                    value: testPass,
+                    touched,
+                    error
+                }
+            }
+        }
+        return shallow(<UserForm {...props} />)
+    }
     it('Renders without crashing', () => {
-        shallow(<UserForm />);
+        buildSubject();
+    });
+
+    it('Should fire the login action when the form is submitted', () => {
+        const wrapper = buildSubject();
+        wrapper.find('form').simulate('submit');
+        expect(callback).toHaveBeenCalled();
     });
 });
