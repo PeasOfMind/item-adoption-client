d7a5525b122f7b58b159d764761d8abe980240ee
Yeqing Liu <40677349+PeasOfMind@users.noreply.github.com>
2019-03-20 08:35:32 -0400

    reduced text on landing page

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -23,7 +23,7 @@ export function LandingPage(props){
             </section>
             <section className="app-description">
                 <h2>But habits can change.</h2>
-                <p>With Item Adoption, take the things that you were about to throw away and find ways to repurpose them, find places to donate them, or give/sell to others in your area. You can also look for items that are used and give them a new home!</p>
+                <p>With Item Adoption, take the things that you were about to throw away and give or sell to others in your area. You can also look for items that are used and give them a new home!</p>
             </section>
             <section className="direct-to-signup">
             <Link to="/register"><button type="button" id="button-to-signup">Sign up to get started</button></Link>
